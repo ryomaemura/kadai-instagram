f5ef3540e8f2511de5a748cc812a70a27820c870
ryomaemura <ryomaemura34@gmail.com>
2020-12-18 19:10:06 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 前村龍 on 2020/12/18.
+//  Copyright © 2020 ryomaemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
